82795600b1656c8ce55ecae12e98f28d1a6c9d1b
Tomy Maulana <stealthflanker93@gmail.com>
2026-09-10 18:25:38 +0700

    style(account): adjust icon size in sidebar navigation

--- resources/js/layouts/account/Layout.vue
+++ resources/js/layouts/account/Layout.vue
@@ -51,7 +51,7 @@ const { isCurrentOrParentUrl } = useCurrentUrl();
                         as-child
                     >
                         <Link :href="item.href">
-                            <component :is="item.icon" class="h-4 w-4" />
+                            <component :is="item.icon" class="size-4" />
                             {{ item.title }}
                         </Link>
                     </Button>
